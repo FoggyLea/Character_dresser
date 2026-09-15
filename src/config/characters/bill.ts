4c1f3e4dc7bc48b02885244af883a92e1cad394a
FoggyLea <franjoea@student.21-school.ru>
2026-09-15 19:28:37 +0300

    new jacket

--- src/config/characters/bill.ts
+++ src/config/characters/bill.ts
@@ -168,10 +168,7 @@ export const BillWeasley: CharacterConfig = {
             src: 'assets/characters/bill/clothes/tops/tshirt.PNG',
             icon: 'assets/characters/bill/icons/tshirt.png',
             defaultZIndex: 65,
-            isRemovable: true,
-            variants: {
-                jacket: 'assets/characters/bill/clothes/tops/tshirt_jacket.PNG'
-            }
+            isRemovable: true
         },
         {
             id: 'earring1',
